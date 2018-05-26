3cf08dbe225ceca777801387a95f633acb9f16c5
ashmitajain09 <ashmitajain@gmail.com>
2018-05-26 18:29:38 +0530

    Update graph.php

--- php-graph-sdk-5.x/graph.php
+++ php-graph-sdk-5.x/graph.php
@@ -2,6 +2,19 @@
 
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
-echo "gfghjkl"
+
+$fb = new Facebook\Facebook([
+  'app_id'                => '2099205980363929',
+  'app_secret'            => 'f8027b5cee91e363086b4e73c228d729',
+  'default_graph_version' => 'v2.10',
+]);
+try {
+  $me = (new FacebookRequest(
+    $session, 'GET', '/me'
+  ))->execute()->getGraphObject(GraphUser::className());
+  echo $me->getName();
+} catch (\Exception $e) {
+  // Some other error occurred
+}
   
   ?>

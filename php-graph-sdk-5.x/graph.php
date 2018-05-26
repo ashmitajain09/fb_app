e59329a6989de0d29722771907b940854425400a
ashmitajain09 <ashmitajain@gmail.com>
2018-05-26 18:18:29 +0530

    Update graph.php

--- php-graph-sdk-5.x/graph.php
+++ php-graph-sdk-5.x/graph.php
@@ -1,31 +1,5 @@
 <?php
-require_once( 'vendor/autoload.php' );
 
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-
-FacebookSession::setDefaultApplication('2099205980363929','f8027b5cee91e363086b4e73c228d729');
-
-// Use one of the helper classes to get a FacebookSession object.
-//   FacebookRedirectLoginHelper
-//   FacebookCanvasLoginHelper
-//   FacebookJavaScriptLoginHelper
-// or create a FacebookSession with a valid access token:
-echo "here";die;
-$session = new FacebookSession('abc');
-
-// Get the GraphUser object for the current user:
-
-try {
-  $me = (new FacebookRequest(
-    $session, 'GET', '/me'
-  ))->execute()->getGraphObject(GraphUser::className());
-  echo $me->getName();
-} catch (FacebookRequestException $e) {
-  // The Graph API returned an error
-} catch (\Exception $e) {
-  // Some other error occurred
-}
-?>
+echo "gfghjkl"
+  
+  ?>
